16b33516bc2d7d02254b10ea5e1bf536c5e136f7
Nitesh Wayafalkar <nitesh.wayafalkar@gmail.com>
2020-10-08 17:37:29 +0000

    added facebook-integration code

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,13 @@
-//This program willprint simple message
+
+
+i//This program willprint simple message
+
+
+class FacebookIntegration {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Welcome to GIT");
+    }
 
 
 class HelloWorld {
